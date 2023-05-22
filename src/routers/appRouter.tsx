a9250ee702963741999adfc86dc9a5eba5447f9c
Fede Otálvares <fnotalvares@gmail.com>
2023-05-22 22:30:02 +0200

    Add login path to appRouter

--- src/routers/appRouter.tsx
+++ src/routers/appRouter.tsx
@@ -1,6 +1,7 @@
 import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../components/App/App";
-import LoginPage from "../pages/loginPage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import HomePage from "../pages/HomePage/HomePage";
 
 const routes: RouteObject[] = [
   {
@@ -9,6 +10,7 @@ const routes: RouteObject[] = [
     children: [
       { index: true, element: <Navigate to={"/user/login"} replace /> },
       { path: "user/login", element: <LoginPage /> },
+      { path: "user/home", element: <HomePage /> },
     ],
   },
 ];
